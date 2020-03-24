e5b7e1f9d36c3684f40a96009c9921214b91a072
connor <connor.chaplain@pipemind.com>
2020-03-24 16:39:45 +0000

    changed navigation

--- components/layout/navigationBar/index.js
+++ components/layout/navigationBar/index.js
@@ -21,33 +21,54 @@ export function NavigationBar (props) {
   const {
     active = ''
   } = props
-  console.log(13, active)
   const [menuActive, setMenuActive] = useState(false)
   return (
     <div className={styles.main}>
-      <Link href='/'>
-        <img src={LOGO} />
-      </Link>
-      <div className={classNames(styles['menu_bar'], styles[menuActive ? 'active' : undefined])}>
-        <ul>
-          {links.map(({name, link}, index) => 
-            <li key={index}>
-              <Link href={link}>
-                <a style={{ color: active === name.toLowerCase() ? 'red' : '' }}>{name}</a>
-              </Link>
+      {/* desktop navigation */}
+      <div className={styles['menu_bar_desktop']}>
+        <Link href='/'>
+          <img src={LOGO} />
+        </Link>
+        <div className={styles['menu_content']}>
+          <ul>
+            {links.map(({name, link}, index) => 
+              <li key={index}>
+                <Link href={link}>
+                  <a style={{ color: active === name.toLowerCase() ? 'red' : '' }}>{name}</a>
+                </Link>
+              </li>
+            )}
+            <li>
+              <a onClick={() => window.scrollTo(0,document.body.scrollHeight)}>CONTACT</a>
             </li>
-          )}
-          <li>
-            <a onClick={() => window.scrollTo(0,document.body.scrollHeight)}>CONTACT</a>
-          </li>
-        </ul>
-        <Footer className={styles['mobile_footer']} />
+          </ul>
+        </div>
       </div>
-      <HamburgerElastic 
-        className={styles[menuActive ? 'active' : undefined]} 
-        isActive={menuActive} 
-        onClick={() => setMenuActive(!menuActive)} 
-      />
+
+
+      {/* mobile navigation */}
+      <div className={styles['menu_bar_mobile']}>
+        <Link href='/'>
+          <img src={LOGO} />
+        </Link>
+        <div className={classNames(styles['menu_bar'], styles[menuActive ? 'active' : undefined])}>
+          <ul>
+            {links.map(({name, link}, index) => 
+              <li key={index}>
+                <Link href={link}>
+                  <a style={{ color: active === name.toLowerCase() ? 'red' : '' }}>{name}</a>
+                </Link>
+              </li>
+            )}
+          </ul>
+          <Footer className={styles['mobile_footer']} />
+        </div>
+        <HamburgerElastic 
+          className={styles[menuActive ? 'active' : undefined]} 
+          isActive={menuActive} 
+          onClick={() => setMenuActive(!menuActive)} 
+        />
+      </div>
     </div>
   )
 }
