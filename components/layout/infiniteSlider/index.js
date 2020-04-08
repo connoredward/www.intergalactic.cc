49c24ad19887852f7d3566402eda5ed04cf54f21
connor <connor.chaplain@pipemind.com>
2020-04-08 09:57:56 +0100

    using custom crarousel

--- components/layout/infiniteSlider/index.js
+++ components/layout/infiniteSlider/index.js
@@ -2,41 +2,117 @@ import React, { useState, useEffect, useRef } from 'react'
 
 import { Carousel } from 'react-responsive-carousel'
 
+
+
+import styles from './styles.scss'
+
+
 export function InfiniteSlider({data}) {
   const sliderRef = useRef()
   const videoArrayRefs = useRef(data.map(() => React.createRef()))
+
+  const [direction, setDirection] = useState()
+
+  const [carouselStyling, setCourselStyling] = useState('flex-start')
+  const [sliderTransform, setSliderTransform] = useState('0%')
+  const [sliderTransition, setSliderTransition] = useState('all 1s')
+
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  useEffect(() => {
+    if (sliderRef.current) {
+      sliderRef.current.addEventListener('transitionend', function () {
+        if (direction === 1) {
+          sliderRef.current.prepend(sliderRef.current.lastElementChild)
+        } else {
+          sliderRef.current.appendChild(sliderRef.current.firstElementChild)
+        }
+        setSliderTransition('none')
+        setSliderTransform('0%')
+        setTimeout(() => {
+          setSliderTransition('all 1s')
+        }, 100)
+      })
+    }
+  }, [sliderRef])
+
+  function nextClick() {
+    const newIndex = currentSlide === 2 ? 0 : currentSlide + 1
+    setCurrentSlide(newIndex)
+    videoArrayRefs.current[currentSlide].current.pause()
+    videoArrayRefs.current[currentSlide].current.currentTime = 0
+    videoArrayRefs.current[currentSlide].current.load()
   
-  function onChange(index,b,c) {
-    videoArrayRefs.current[0].current.pause()
-    console.log(videoArrayRefs.current[0].current)
-    console.log('index', index)
-    console.log(videoArrayRefs)
-    videoArrayRefs.current[index].current.pause()
-    videoArrayRefs.current[index].current.currentTime = 0
-    videoArrayRefs.current[index].current.load()
+    setDirection(-1)
+    setCourselStyling('flex-start')
+    setSliderTransform('-20%')
   }
-  function onClickItem(a,b,c) {
-    console.log(a,b,c)
-  }
-  function onClickThumb(a,b,c) {
-    console.log(a,b,c)
+
+  function prevClick() {
+    const newIndex = currentSlide === 0 ? 2 : currentSlide - 1
+    setCurrentSlide(newIndex)
+    videoArrayRefs.current[currentSlide].current.pause()
+    videoArrayRefs.current[currentSlide].current.currentTime = 0
+    videoArrayRefs.current[currentSlide].current.load()
+
+    if (direction === -1) {
+      setDirection(1)
+      sliderRef.current.appendChild(sliderRef.current.firstElementChild)
+    }
+    setCourselStyling('flex-end')
+    setSliderTransform('20%')
   }
 
   return (
-    <Carousel 
-      showArrows={true} 
-      infiniteLoop={true}
-      onChange={onChange} 
-      showThumbs={false}
-      // onClickItem={onClickItem} 
-      // onClickThumb={onClickThumb}
-    >
-      {data.map(({videoSrc}, index) => 
-        <div key={index}>
-          <video src={videoSrc} autoPlay muted ref={videoArrayRefs.current[index]} />
+    <div>
+
+      <div className={styles.container}>
+        <div className={styles.carousel} style={{ justifyContent: carouselStyling }}>
+          <div ref={sliderRef} className={styles.slider} style={{ transform: `translate(${sliderTransform})`, transition: sliderTransition }}>
+            {data.map(({videoSrc}, index) => 
+              <section>
+                <video src={videoSrc} autoPlay muted ref={videoArrayRefs.current[index]} onEnded={() => nextClick()} />
+              </section>
+            )}
+          </div>
+          <div className={styles.controls}>
+            <button className={styles.next} onClick={() => nextClick()}>keyboard_arrow_right</button>
+            <button className={styles.prev} onClick={() => prevClick()}>keyboard_arrow_left</button>
+          </div>
         </div>
-      )}
-    </Carousel>
+</div>
+
+
+
+      {/* <Carousel 
+        className={styles['desktop_carousel']}
+        ref={sliderRef}
+        showArrows={true} 
+        infiniteLoop={true}
+        onChange={onChange} 
+        showThumbs={false}
+        transitionTime={speed}
+        dots={false}
+      >
+        {data.map(({videoSrc}, index) => 
+          <div className={styles['slide_wrapper']} key={index}>
+            <video src={videoSrc} autoPlay muted ref={videoArrayRefs.current[index]} onEnded={() => nextSlide(index)} />
+          </div>
+        )}
+      </Carousel> */}
+
+      {/* <Carousel 
+        axis="vertical" 
+        verticalSwipe="natural" 
+        emulateTouch
+      >
+        {data.map(({videoSrc}, index) => 
+          <div className={styles['slide_wrapper']} key={index}>
+            <video src={videoSrc} autoPlay muted ref={videoArrayRefs.current[index]} />
+          </div>
+        )}
+      </Carousel> */}
+    </div>
   )
 }
 
