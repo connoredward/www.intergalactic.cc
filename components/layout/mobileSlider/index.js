4647c7c482af4e8c5bc324380ce7b153cf151a0f
connor <connor.chaplain@pipemind.com>
2020-04-10 09:10:44 +0100

    using react slick for mobile

--- components/layout/mobileSlider/index.js
+++ components/layout/mobileSlider/index.js
@@ -1,50 +1,37 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-// import { Carousel } from 'react-responsive-carousel'
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-
+import Slider from 'react-slick'
+import classNames from 'classnames'
 
 import styles from './styles.scss'
 
-export function MobileSlider({data, className}) {
-  function nextSlide() {
-    console.log('next')
+const settings = {
+  dots: false,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  vertical: true,
+  verticalSwiping: true,
+  arrows: false,
+  beforeChange: (currentSlide,  nextSlide) => {
+    console.log(currentSlide, nextSlide)
+  },
+  afterChange: (currentSlide) => {
+    console.log(currentSlide)
   }
+}
+
+export function MobileSlider({data, className}) {
+  const sliderRef = useRef()
 
   return (
-    <CarouselProvider
-      className={className}
-      naturalSlideWidth={100}
-      naturalSlideHeight={125}
-      totalSlides={1}
-      orientation='vertical'
-    >
-      <Slider>
-      {data.map(({videoSrc, titleimg}, index) => 
-        <Slide key={index}>
-          <div className={styles['slide_wrapper']}>
-            <img src={titleimg} />
-            <video src={videoSrc} autoPlay muted onEnded={() => nextSlide()} />
-          </div>
-        </Slide>
+    <Slider {...settings} className={classNames(styles['slider_wrapper'], className)}>
+      {data.map(({videoSrc}, index) => 
+        <div key={index} className={styles['slide_content']}>
+          <video src={videoSrc} autoPlay muted />
+        </div>
       )}
-      </Slider>
-      <ButtonBack>Back</ButtonBack>
-      <ButtonNext>Next</ButtonNext>
-    </CarouselProvider>
-    // <Carousel
-    //   axis='vertical'
-    //   verticalSwipe='natural'
-    //   emulateTouch
-    //   className={className}
-    // >
-    //   {data.map(({videoSrc, titleimg}, index) => 
-    //     <div className={styles['slide_wrapper']} key={index}>
-    //       <img src={titleimg} />
-    //       <video src={videoSrc} autoPlay muted onEnded={() => nextSlide()} />
-    //     </div>
-    //   )}
-    // </Carousel>
+    </Slider>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import './global.scss'
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 
 class Base extends App {
@@ -12,8 +14,8 @@ class Base extends App {
     const { Component, pageProps } = this.props
     return (
       <>
-        <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        {/* <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" /> */}
         <Component {...pageProps} />
       </>
     )
