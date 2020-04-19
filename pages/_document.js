e194c798d503f50d6044393d437f1c1008662321
connor <connor.chaplain@pipemind.com>
2020-04-19 14:21:15 +0100

    changed faviconn

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,10 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"></meta>
+          <link rel="apple-touch-icon" sizes="180x180" href="../static/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="../static/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="../static/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="../static/favicon/site.webmanifest"></link>
         </Head>
         <body>
           <Main />
