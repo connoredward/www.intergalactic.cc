3e8334c03c31c6ff99da570239f516181c8e9296
connor <connor.chaplain@pipemind.com>
2020-04-21 19:32:46 +0100

    adding side scroll thing

--- components/layout/HoriScroll/index.js
+++ components/layout/HoriScroll/index.js
@@ -1,14 +1,26 @@
 import { useEffect, useState, useRef } from 'react'
 
+import HorizontalScroll from 'react-scroll-horizontal'
+
 import styles from './styles.scss'
 
-import IMAGE from '~/static/images/catergories/COURTESY_ZOOM.jpg'
+import IMAGE_0 from '~/static/images/test/i0.jpg'
+import IMAGE_1 from '~/static/images/test/i1.jpg'
+import IMAGE_2 from '~/static/images/test/i2.jpg'
+import IMAGE_3 from '~/static/images/test/i3.jpg'
+import IMAGE_4 from '~/static/images/test/i4.jpg'
+import IMAGE_5 from '~/static/images/test/i5.jpg'
+import IMAGE_6 from '~/static/images/test/i6.jpg'
+import IMAGE_7 from '~/static/images/test/i7.jpg'
+
+let imgs = [IMAGE_0, IMAGE_1, IMAGE_2, IMAGE_3, IMAGE_4, IMAGE_5, IMAGE_6, IMAGE_7]
 
 export function HoriScroll () {
-
   return (
     <div className={styles.main}>
-      fdddd
+      <HorizontalScroll reverseScroll={true}>
+        {imgs.map((item, index) => <img key={index} src={item} />)}
+      </HorizontalScroll>
     </div>
   )
 }
