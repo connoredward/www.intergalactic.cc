3ebb5180b3a8aa2fd134322693951757668fbdbe
connor <connor.chaplain@pipemind.com>
2020-04-22 16:27:35 +0100

    fixed nav bar

--- components/layout/mobileSlider/index.js
+++ components/layout/mobileSlider/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Slider from 'react-slick'
 import classNames from 'classnames'
@@ -8,6 +8,7 @@ import styles from './styles.scss'
 
 export function MobileSlider({data, className, onClick}) {
   const [currentSlide, setCurrentSlide] = useState(0)
+  const [mobileHeight, setMobileHeight] = useState()
 
   const settings = {
     dots: false,
@@ -18,19 +19,33 @@ export function MobileSlider({data, className, onClick}) {
     verticalSwiping: true,
     arrows: false,
     afterChange: afterChange,
-    autoplay: true,
-    autoplaySpeed: 2000,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     speed: 500
   }
+
+  // useEffect(() => {
+  //   if (window) {
+  //     setMobileHeight(window.innerHeight)
+  //     console.log(window.innerHeight)
+  //   }
+  // }, [])
 
   function afterChange(currentSlide) {
     setCurrentSlide(currentSlide)
   }
 
   return (
-    <Slider {...settings} className={classNames(styles['slider_wrapper'], className)}>
+    <Slider style={{}} {...settings} className={classNames(styles['slider_wrapper'], className)}>
       {data.map(({imgSrc, titleImg, slug}, index) =>  
-        <div key={index} className={styles['slide_content']} onClick={() => onClick(slug)}>
+        <div 
+          style={{ height: 'calc(200vh - 60px)', background: 'red' }}
+          key={index} 
+          className={styles['slide_content']} 
+          onClick={() => onClick(slug)}
+          // style={{ minHeight: `calc(90vh - 60px)`, height: `calc(${mobileHeight}px - 60px)`}}
+          // style={{ height: '100px !important' }}
+        >
           <div className={styles['background_image']} style={{ backgroundImage: `url(${imgSrc})` }} />
           <img src={titleImg} className={styles[currentSlide === index ? 'active' : undefined]} />
         </div>
