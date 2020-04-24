aa2af774e1e0e8c62bcd2e1a15ebf36b2c4cd3f3
connor <connor.chaplain@pipemind.com>
2020-04-24 08:19:48 +0100

    chamged back mobile slider

--- components/layout/mobileSlider/index.js
+++ components/layout/mobileSlider/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 import Slider from 'react-slick'
 import classNames from 'classnames'
@@ -8,7 +8,6 @@ import styles from './styles.scss'
 
 export function MobileSlider({data, className, onClick}) {
   const [currentSlide, setCurrentSlide] = useState(0)
-  const [mobileHeight, setMobileHeight] = useState()
 
   const settings = {
     dots: false,
@@ -19,20 +18,10 @@ export function MobileSlider({data, className, onClick}) {
     verticalSwiping: true,
     arrows: false,
     afterChange: afterChange,
-    // autoplay: true,
-    // autoplaySpeed: 2000,
+    autoplay: true,
+    autoplaySpeed: 2000,
     speed: 500
   }
-
-  useEffect(() => {
-    if (window) {
-      setMobileHeight(window.innerHeight)
-      window.addEventListener('resive', function () {
-        setMobileHeight(window.innerHeight)
-      })
-      console.log(window.innerHeight)
-    }
-  }, [])
 
   function afterChange(currentSlide) {
     setCurrentSlide(currentSlide)
@@ -45,12 +34,9 @@ export function MobileSlider({data, className, onClick}) {
           key={index} 
           className={styles['slide_content']} 
           onClick={() => onClick(slug)}
-          >
-          <div style={{ height: `calc(${mobileHeight}px - 60px)` }}>
-            {/* <h1 style={{ color: 'white' }}>{mobileHeight}</h1> */}
-            <div className={styles['background_image']} style={{ backgroundImage: `url(${imgSrc})` }} />
-            <img src={titleImg} className={styles[currentSlide === index ? 'active' : undefined]} />
-          </div>
+        >
+          <div className={styles['background_image']} style={{ backgroundImage: `url(${imgSrc})` }} />
+          <img src={titleImg} className={styles[currentSlide === index ? 'active' : undefined]} />
         </div>
       )}
     </Slider>
