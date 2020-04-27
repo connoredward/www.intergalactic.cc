75b1ace01abef36207caf7588126c700e734b1d5
connor <connor.chaplain@pipemind.com>
2020-04-27 08:43:55 +0100

    connected brandded

--- pages/branded/index.js
+++ pages/branded/index.js
@@ -6,36 +6,57 @@ import Head from 'next/head'
 import PageWrapper from '~/components/layout/pageWrapper'
 import DirectorCard from '~/components/layout/directorCard'
 import VideoGrid from '~/components/layout/videoGrid'
+import VideoModal from '~/components/layout/videoModal'
 
-import { wordpressCardApi } from '~/components/modules/wordpressCall'
-
-import styles from './styles.scss'
+import { getPage, getVimeoModalUrl } from '~/api/wordpress'
 
 export function BrandedPage (props) {
   const {v = ''} = props
   
   const [brandedList, setBrandedList] = useState([])
+  const [modalState, setModalState] = useState({open: false, data: {}})
 
   useEffect(() => {
     onLoad()
+    if (v) startVideo(v)
+    Router.events.on('routeChangeComplete', (url) => {
+      const videoUrl = url.split('v=')[1]
+      if (videoUrl) startVideo(videoUrl)
+      else setModalState({open: false, data: {}})
+    })
   }, [])
   
   async function onLoad() {
-    setBrandedList(await wordpressCardApi('branded'))
+    setBrandedList(await getPage('branded'))
+  }
+
+  async function startVideo(videoSlug) {
+    setModalState({open: true, data: await getVimeoModalUrl(videoSlug)})
+  }
+
+  async function changeRoute(videoSlug) {
+    const href = `/branded?v=${videoSlug}`
+    Router.push('/branded', href, { shallow: true })
+    setModalState({open: true, data: await getVimeoModalUrl(videoSlug)})
+  }
+
+  function closeModal() {
+    const href = '/branded'
+    Router.push(href, href, { shallow: true })
+    setModalState({open: false, data: {}})
   }
 
   return (
     <PageWrapper active={'branded'}>
-      <Head>
-        <title>Intergalactic &ndash; Branded</title>
-      </Head>
-      <VideoGrid gridType={'flexGrid'}>
+      <Head><title>Intergalactic &ndash; Branded</title></Head>
+      <VideoGrid>
         {brandedList.map((item, index) => 
-          <DirectorCard {...item} key={index}>
-            <img src={item.titleImg} />
+          <DirectorCard {...item} key={index} onClick={() => changeRoute(item.slug)}>
+            <img src={item.imgTitleSrc} />
           </DirectorCard>
         )}
       </VideoGrid>
+      <VideoModal openModal={modalState} closeModal={() => closeModal()} />
     </PageWrapper>
   )
 }
