e96f51ad3b7ea449271fdaa61428f521b5f82bf0
connor <connor.chaplain@pipemind.com>
2020-05-15 14:10:00 +0100

    fsd

--- components/layout/mobileSlider/index.js
+++ components/layout/mobileSlider/index.js
@@ -13,7 +13,7 @@ export function MobileSlider({data, className, onClick}) {
   useEffect(() => {
     if (window) {
       if(navigator?.userAgent.includes('Instagram')){
-        setHeight('500px')
+        setHeight(`${window.innerHeight}px`)
         // window.location.href = 'https://www.intergalacticstudios.com/'
       } else {
         setHeight(`calc(${window.innerHeight}px - 60px)`)
