b4fcd0d533adc997813321699cd1a9f6cf5b55ff
connor <connor.chaplain@pipemind.com>
2020-05-15 18:15:52 +0100

    df

--- components/layout/mobileSlider/index.js
+++ components/layout/mobileSlider/index.js
@@ -12,15 +12,15 @@ export function MobileSlider({data, className, onClick}) {
 
   useEffect(() => {
     if (window) {
-      if(navigator?.userAgent.includes('Instagram')){
-          setHeight(`100vh`)
-      } 
-      else {
+      // if(navigator?.userAgent.includes('Instagram')){
+      //     setHeight(`100vh`)
+      // } 
+      // else {
+      setHeight(`calc(${window.innerHeight}px - 60px)`)
+      window.addEventListener('resize', function () {
         setHeight(`calc(${window.innerHeight}px - 60px)`)
-        window.addEventListener('resize', function () {
-          setHeight(`calc(${window.innerHeight}px - 60px)`)
-        })
-      }
+      })
+      // }
     }
   }, [])
 
